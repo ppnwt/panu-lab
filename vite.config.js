57f17a57b40ae8aa72d65e599d1ad65556c937c0
New Panuwat <new_sbh@me.com>
2022-12-07 22:05:38 +0700

    npm updated & remove unused file

--- vite.config.js
+++ vite.config.js
@@ -20,18 +20,6 @@ export default defineConfig({
             new CustomEvent("swUpdated", { detail: registration })
           );
         },
-        icons: [
-          {
-            src: "pwa-192x192.jpg",
-            sizes: "192x192",
-            type: "image/jpg",
-          },
-          {
-            src: "pwa-512x512.jpg",
-            sizes: "512x512",
-            type: "image/jpg",
-          },
-        ],
       },
     }),
   ],
