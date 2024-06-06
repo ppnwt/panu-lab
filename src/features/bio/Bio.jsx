7963b386a5e5faba022ef31769473109b1190e66
New Panuwat <t_panuwat.boonrod@scb.co.th>
2024-06-06 14:08:27 +0700

    Update current status

--- src/features/bio/Bio.jsx
+++ src/features/bio/Bio.jsx
@@ -1,4 +1,11 @@
-import { Avatar, Text, ListItem, UnorderedList, VStack, Divider } from "@chakra-ui/react";
+import {
+  Avatar,
+  Text,
+  ListItem,
+  UnorderedList,
+  VStack,
+  Divider,
+} from "@chakra-ui/react";
 
 import { avatarPhoto } from "../../utils/api/images";
 
@@ -9,7 +16,7 @@ export default function Life({ bioData }) {
       <Text as="p" fontSize={"xl"} fontWeight="bold">
         Mr. Panuwat Boonrod (New)
       </Text>
-      <Text fontSize={"xl"}>Software Developer @ True IDC</Text>
+      <Text fontSize={"xl"}>Software Engineer</Text>
       <UnorderedList>
         {bioData?.map((data) => (
           <ListItem key={data.id}>{data.label}</ListItem>
